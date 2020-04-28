c012911b8c60e6fd6861bad3a3688067441139da
J. Christian Romero <jcromerohdz@gmail.com>
2020-04-28 16:50:21 -0700

    Coordinates, zoom, Theme

--- AdvancedVisualization/advancedVisualization.R
+++ AdvancedVisualization/advancedVisualization.R
@@ -172,6 +172,84 @@ w + geom_point(aes(size=BudgetMillions)) +
   geom_smooth() + 
   facet_grid(Genre~Year)
 
+#------------ Coordinates
+# Limits
+m <- ggplot(movies, aes(x=CriticRating, y=AudienceRating,
+                        size=BudgetMillions,
+                        color=Genre))
 
+m + geom_point()
 
+m + geom_point() + xlim(50,100) + ylim(50, 100)
 
+# No work well always
+
+n <- ggplot(movies, aes(x=BudgetMillions))
+n + geom_histogram(binwidth = 10, aes(fill=Genre), color="Black")
+n + geom_histogram(binwidth = 10, aes(fill=Genre), color="Black") + ylim(0, 50)
+
+#instead -> zoom
+n + geom_histogram(binwidth = 10, aes(fill=Genre), color="Black") +
+  coord_cartesian(ylim=c(0, 50))
+
+w + geom_point(aes(size=BudgetMillions)) + 
+  geom_smooth() + 
+  facet_grid(Genre~Year) + 
+  coord_cartesian(ylim=c(0,100))
+
+#-------------- Theme
+o <- ggplot(movies, aes(x=BudgetMillions))
+h <- o + geom_histogram(binwidth = 10, aes(fill=Genre), color="Black")
+
+h
+
+#axes labels
+h + xlab("Money Axis") + ylab("Number of Movies")
+
+#label formatting
+h +
+  xlab("Money Axis") +
+  ylab("Number of Movies") +
+  theme(axis.title.x = element_text(color = "DarkGreen", size=30),
+        axis.title.y = element_text(color = "Red", size=30))
+
+#tick mark formatting
+h +
+  xlab("Money Axis") +
+  ylab("Number of Movies") +
+  theme(axis.title.x = element_text(color = "DarkGreen", size=30),
+        axis.title.y = element_text(color = "Red", size=30),
+        axis.text.x = element_text(size = 20),
+        axis.text.y = element_text(size = 20))
+ 
+?theme
+
+#Legend formatting
+h +
+  xlab("Money Axis") +
+  ylab("Number of Movies") +
+  theme(axis.title.x = element_text(color = "DarkGreen", size=30),
+        axis.title.y = element_text(color = "Red", size=30),
+        axis.text.x = element_text(size = 20),
+        axis.text.y = element_text(size = 20),
+        legend.title = element_text(size = 30),
+        legend.text = element_text(size = 20),
+        legend.position = c(1,1),
+        legend.justification = c(1,1))
+
+#Title of the plot
+h +
+  xlab("Money Axis") +
+  ylab("Number of Movies") +
+  ggtitle("Movie Budget Distribution") +
+  theme(axis.title.x = element_text(color = "DarkGreen", size=30),
+        axis.title.y = element_text(color = "Red", size=30),
+        axis.text.x = element_text(size = 20),
+        axis.text.y = element_text(size = 20),
+        legend.title = element_text(size = 30),
+        legend.text = element_text(size = 20),
+        legend.position = c(1,1),
+        legend.justification = c(1,1),
+        plot.title = element_text(color = "DarkBlue",
+                                  size = 25,
+                                  family = "Courier"))
