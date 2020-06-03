e13405506ebba727cd61d9397754ac6ef48c4229
J. Christian Romero <jcromerohdz@gmail.com>
2020-06-03 16:14:25 -0700

    Adding k-means

---
+++ MachineLearning/K-Means/k_means_clustering.R
@@ -0,0 +1,41 @@
+# K-Means Clustering
+# Set our workspace
+getwd()
+setwd("/home/chris/Documents/itt/Enero_Junio_2020/Mineria_de_datos/DataMining/MachineLearning/K-Means")
+getwd()
+
+# Importing the dataset
+dataset = read.csv('Mall_Customers.csv')
+dataset = dataset[4:5]
+
+
+# Using the elbow method to find the optimal number of clusters
+set.seed(6)
+wcss = vector()
+for (i in 1:10) wcss[i] = sum(kmeans(dataset, i)$withinss)
+plot(1:10,
+     wcss,
+     type = 'b',
+     main = paste('The Elbow Method'),
+     xlab = 'Number of clusters',
+     ylab = 'WCSS')
+
+# Fitting K-Means to the dataset
+set.seed(29)
+kmeans = kmeans(x = dataset, centers = 5)
+y_kmeans = kmeans$cluster
+
+# Visualising the clusters
+# install.packages('cluster')
+library(cluster)
+clusplot(dataset,
+         y_kmeans,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Clusters of customers'),
+         xlab = 'Annual Income',
+         ylab = 'Spending Score')
